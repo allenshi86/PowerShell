d998659dd96d6e54b65476b9b50a0934beab60f9
allenshi86 <zhenning.shi@163.com>
2020-08-27 18:21:55 +0800

    Update check_account_auth_failure.ps1

--- check_account_auth_failure.ps1
+++ check_account_auth_failure.ps1
@@ -11,7 +11,7 @@ $cred =  New-Object System.Management.Automation.PSCredential($username,$passwor
 #将命令执行结果转化为字符形式才能作为邮件body体。
 #使用BadLogonCount 和badPwdCount 都可以。
 
-$body = Get-ADUser -SearchBase $OUPATH -Properties * -Filter {BadLogonCount -ge 15 -and Enabled -eq "True"} | Format-Table DisplayName,samAccountName,AccountLockoutTime,BadLogonCount -AutoSize  | Out-String
+$body = Get-ADUser -SearchBase $oupath -Properties * -Filter {BadLogonCount -ge 15 -and Enabled -eq "True"} | Format-Table DisplayName,samAccountName,AccountLockoutTime,BadLogonCount -AutoSize  | Out-String
 
 Send-MailMessage  -From 'wechat_it@132.com' -To  'user1@132.com','user1@132.com','user3@132.com' -Subject "AD账号监测-认证失败次数【$starttime-$nowtime】 "  -SmtpServer 'mail.132.com' -Body $body -Credential $cred -Encoding 'utf8'
  
