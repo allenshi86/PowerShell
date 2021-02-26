7d88ca49aa07dc5a04e5d2ef1f94165c481aec3e
allenshi86 <zhenning.shi@163.com>
2021-02-26 10:56:12 +0800

    add delete_email.ps1

---
+++ Exchange/delete_email.ps1
@@ -0,0 +1,8 @@
+﻿Add-PSSnapin microsoft.exchange*
+
+$usermailbox = Read-Host "请输入目标邮箱:"
+$keywords = Read-Host "请输入主题或内容关键字:"
+
+
+Search-Mailbox -Identity $usermailbox -SearchQuery "Subject:$keywords OR $keywords"  -DeleteContent -force
+
