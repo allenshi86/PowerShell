01beb13c7e5e1286deefc68525685966c3f801ea
allenshi86 <zhenning.shi@163.com>
2021-04-09 14:01:13 +0800

    add clearlog_iis.ps1

---
+++ clearlog_iis.ps1
@@ -0,0 +1,11 @@
+﻿
+$FilePath = "C:\inetpub\logs\LogFiles\W3SVC1","C:\inetpub\logs\LogFiles\W3SVC1","C:\inetpub\logs\LogFiles\IMAPLOG","C:\inetpub\logs\LogFiles\POPLOG"
+$DaysAgo=2
+$Today=Get-Date
+
+foreach ($i in $FilePath)
+{ 
+    if(test-path $i){
+        Get-ChildItem -Path  $i "*.log" | Where-Object {$_.LastWriteTime -lt $Today.AddDays(-$DaysAgo)} |Remove-Item
+    }
+}
